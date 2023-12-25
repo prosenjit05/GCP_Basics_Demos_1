4b616d79645c38e37521bb927bca2c51ef9a05fb
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-12-25 11:24:08 +0530

    initial commit

---
+++ GCP-CloudFunction-SB-TF/src/main/java/com/example/GCPCloudFunctionSBTF/GcpCloudFunctionSbTfApplication.java
@@ -0,0 +1,13 @@
+package com.example.GCPCloudFunctionSBTF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpCloudFunctionSbTfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpCloudFunctionSbTfApplication.class, args);
+	}
+
+}
