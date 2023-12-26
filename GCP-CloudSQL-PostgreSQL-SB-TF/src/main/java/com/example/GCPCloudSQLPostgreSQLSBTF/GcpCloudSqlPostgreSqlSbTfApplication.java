0b44f76de136e9a6e107e93ac0dc719e056e587d
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-12-26 11:50:05 +0530

    initial commit

---
+++ GCP-CloudSQL-PostgreSQL-SB-TF/src/main/java/com/example/GCPCloudSQLPostgreSQLSBTF/GcpCloudSqlPostgreSqlSbTfApplication.java
@@ -0,0 +1,13 @@
+package com.example.GCPCloudSQLPostgreSQLSBTF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpCloudSqlPostgreSqlSbTfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpCloudSqlPostgreSqlSbTfApplication.class, args);
+	}
+
+}
---
+++ GCP-CloudSQL-PostgreSQL-SB-TF/src/test/java/com/example/GCPCloudSQLPostgreSQLSBTF/GcpCloudSqlPostgreSqlSbTfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GCPCloudSQLPostgreSQLSBTF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class GcpCloudSqlPostgreSqlSbTfApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
