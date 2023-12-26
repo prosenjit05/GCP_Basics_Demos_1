c3fd4568a0ee8d49d34c00df5c092a6c25f6eda3
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-12-26 13:40:39 +0530

    initial commit

---
+++ GCP-Firestore-SB-TF/src/main/java/com/example/GCPFirestoreSBTF/GcpFirestoreSbTfApplication.java
@@ -0,0 +1,13 @@
+package com.example.GCPFirestoreSBTF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpFirestoreSbTfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpFirestoreSbTfApplication.class, args);
+	}
+
+}
---
+++ GCP-Firestore-SB-TF/src/test/java/com/example/GCPFirestoreSBTF/GcpFirestoreSbTfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GCPFirestoreSBTF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class GcpFirestoreSbTfApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
