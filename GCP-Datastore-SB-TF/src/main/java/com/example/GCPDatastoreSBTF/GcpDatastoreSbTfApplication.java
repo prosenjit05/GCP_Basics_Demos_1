1c1383882a46592034f96ef77ee545f9aa21287b
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-12-26 20:10:12 +0530

    initial commit

---
+++ GCP-Datastore-SB-TF/src/main/java/com/example/GCPDatastoreSBTF/GcpDatastoreSbTfApplication.java
@@ -0,0 +1,13 @@
+package com.example.GCPDatastoreSBTF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpDatastoreSbTfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpDatastoreSbTfApplication.class, args);
+	}
+
+}
---
+++ GCP-Datastore-SB-TF/src/test/java/com/example/GCPDatastoreSBTF/GcpDatastoreSbTfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GCPDatastoreSBTF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class GcpDatastoreSbTfApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
