58d9626db0a24394d92a2fbb91453012d2af3d29
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-12-27 11:35:31 +0530

    initial commit

---
+++ GCP-pubsub-SB-TF/GCP-pubsub-SB-TF-Consumer/src/main/java/com/example/GCPpubsubSBTFConsumer/GcpPubsubSbTfConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.GCPpubsubSBTFConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpPubsubSbTfConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpPubsubSbTfConsumerApplication.class, args);
+	}
+
+}
---
+++ GCP-pubsub-SB-TF/GCP-pubsub-SB-TF-Consumer/src/test/java/com/example/GCPpubsubSBTFConsumer/GcpPubsubSbTfConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GCPpubsubSBTFConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class GcpPubsubSbTfConsumerApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
